94cce766cb82f20a90f78937b3a94c89db7c6882
Corentin Kerisit <c@42.am>
2015-01-16 00:05:05 +0100

    CRUD for Organization

--- app/modules/organizations/organizations.js
+++ app/modules/organizations/organizations.js
@@ -1,22 +1,90 @@
-var orgs = angular.module('portal.organizations', ['ui.router', 'templates', 'ngAnimate', 'restangular']);
+var organizations = angular.module('portal.organizations', [
+  'ui.router',
+  'templates',
+  'ngAnimate',
+  'restangular']);
 
-orgs.config(function($stateProvider) {
+organizations.config(function($stateProvider) {
 
   $stateProvider.state('organizations', {
     url: '/organizations',
-    controller: 'OrganizationsController',
+    abstract: true,
+    templateUrl: 'modules/organizations/views/main.tpl.html'
+  })
+  .state('organizations.list', {
+    url: '',
+    controller: 'OrganizationsListCtrl',
     templateUrl: 'modules/organizations/views/list.tpl.html',
+  })
+  .state('organizations.view', {
+    url: '/:id',
+    controller: 'OrganizationsViewCtrl',
+    templateUrl: 'modules/organizations/views/view.tpl.html',
+  })
+  .state('organizations.add', {
+    url: '/add',
+    controller: 'OrganizationsAddCtrl',
+    templateUrl: 'modules/organizations/views/form.tpl.html',
+  })
+  .state('organizations.edit', {
+    url: '/:id/edit',
+    controller: 'OrganizationsEditCtrl',
+    templateUrl: 'modules/organizations/views/form.tpl.html',
   });
 
 });
 
-orgs.controller('OrganizationsController', function($scope, $http, $sce, $timeout, Restangular) {
+organizations.controller('OrganizationsListCtrl', function($scope, Restangular) {
 
   var Orgs = Restangular.service('organizations');
 
-  Orgs.getList().then(function(orgs) {
-    $scope.orgs = orgs;
-    $scope.loaded = true;
+  $scope.organizations = Orgs.getList().$object;
+
+});
+
+organizations.controller('OrganizationsViewCtrl', function($scope, $stateParams, Restangular) {
+
+  var Orgs = Restangular.service('organizations');
+
+  Orgs.one($stateParams.id).get().then(function(response) {
+    $scope.organization = response.data;
   });
+
+
+});
+
+organizations.controller('OrganizationsAddCtrl', function($scope, $state, Restangular) {
+
+  var Orgs = Restangular.service('organizations');
+
+  $scope.organization = {};
+
+  $scope.save = function() {
+    console.log('test');
+    Orgs.post($scope.organization).then(function(response) {
+      $state.transitionTo('organizations.list');
+    });
+  };
+
+});
+
+organizations.controller('OrganizationsEditCtrl', function($scope, $state, $stateParams, Restangular) {
+
+  var Orgs = Restangular.service('organizations');
+  var organization = Orgs.one($stateParams.id);
+
+  organization.get().then(function(response) {
+    $scope.organization = response.data;
+  });
+
+  $scope.save = function() {
+    var toSend = _.pick($scope.organization, function(value, key) {
+      return key.charAt(0) != '_' || key == '_etag';
+    });
+
+    organization.patch(toSend).then(function(response) {
+      $state.transitionTo('organizations.list');
+    });
+  };
 
 });
