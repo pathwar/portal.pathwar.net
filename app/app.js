8b9a65e40b527123d85f9a577a076b15856c69d0
Corentin Kerisit <c@42.am>
2015-01-15 12:04:04 +0100

    Configuring EVE API provider

--- app/app.js
+++ app/app.js
@@ -2,15 +2,33 @@ var portal = angular.module('portal', [
   'ngAnimate',
   'ngRoute',
   'templates',
+  'restangular',
   'portal.home',
   'portal.levels',
   'portal.organizations'
   ]);
 
-portal.config(function($locationProvider, $urlRouterProvider) {
+portal.config(function($locationProvider, $urlRouterProvider, RestangularProvider) {
 
   $locationProvider.html5Mode(true);
   $urlRouterProvider.otherwise('/');
+
+  RestangularProvider.setBaseUrl('//'+document.domain+':5000');
+
+  RestangularProvider.setRestangularFields({
+    id: "_id"
+  });
+
+  RestangularProvider.setDefaultHeaders({
+    Authorization: 'Basic cm9vdC10b2tlbjo='
+  });
+
+  RestangularProvider.addResponseInterceptor(function(data, operation, what, url, response, deferred) {
+
+    if (operation == 'getList') return data._items;
+    return response;
+
+  });
 
 });
 
