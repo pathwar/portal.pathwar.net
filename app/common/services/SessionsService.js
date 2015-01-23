be2ad58453feef0163cef0c9fb0ea90cc19773bd
Corentin Kerisit <c@42.am>
2015-01-23 18:04:08 +0100

    WIP og SessionsService

---
+++ app/common/services/SessionsService.js
@@ -0,0 +1,15 @@
+angular
+  .module('portal.services')
+  .factory('SessionsService', function(Restangular) {
+
+    var Sessions = Restangular.all('sessions');
+
+    var service = {}
+
+    service.getSessions = function(opts) {
+      return Sessions.getList();
+    };
+
+    return service;
+
+  });
