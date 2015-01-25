59b6c33dbe8a4d22065c94d610229a623bee9872
Corentin Kerisit <c@42.am>
2015-01-26 00:37:27 +0100

    Adding ability to get a user organizations

--- app/modules/organizations/services/OrganizationsService.js
+++ app/modules/organizations/services/OrganizationsService.js
@@ -10,5 +10,16 @@ angular
     return Organizations.post(org);
   };
 
+  service.getOrganizationsByUserId = function(userId) {
+    return Restangular.all('organization-users').getList({
+      where: JSON.stringify({
+        user: userId
+      }),
+      embedded: JSON.stringify({
+        organization: 1
+      })
+    });
+  };
+
   return service;
 });
