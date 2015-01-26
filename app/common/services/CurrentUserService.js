ecd53cfe9bd70c0c8fec1354fa888d44a02714e2
Corentin Kerisit <c@42.am>
2015-01-26 16:15:12 +0100

    Better CurrentUserService (Fixes #43)

--- app/common/services/CurrentUserService.js
+++ app/common/services/CurrentUserService.js
@@ -1,16 +1,56 @@
 angular
   .module('portal.services')
-  .factory('CurrentUserService', function($window, AuthService) {
+  .factory('CurrentUserService', function($q, $window, AuthService, UsersService, OrganizationsService) {
 
     var service = {};
 
     var storage = {
       authToken: null,
-      user: null,
-      organization: null
+      user: {},
+      organization: {}
     };
 
-    // User Object
+    service.login = function(credentials) {
+
+      var authentificateUser = function(credentials) {
+        return AuthService.login(credentials)
+        .then(function(token) {
+          service.setAuthToken(token.token);
+          return token.user;
+        });
+      };
+
+      return authentificateUser(credentials)
+              .then(service.loadUserInfo);
+    };
+
+    // Persistency
+    service.restore = function() {
+
+      var currentUser = $window.localStorage.getItem('currentUser');
+      storage = JSON.parse(currentUser) || {};
+
+      return returnStorage();
+    };
+
+    service.loadUserInfo = function (userId) {
+      return loadUser(userId)
+        .then(loadOrganizations)
+        .then(returnStorage);
+    }
+
+    service.logout = function() {
+      return AuthService.logout().then(function() {
+        service.clear();
+      });
+    };
+
+    service.clear = function() {
+      $window.localStorage.removeItem('currentUser');
+      storage = {};
+    };
+
+    // User
     service.setUser = function(user) {
       storage.user = user;
       persist();
@@ -21,25 +61,12 @@ angular
     };
 
     // Authentification
-
     service.setAuthToken = function(token) {
       storage.authToken = token;
       persist();
     };
 
     service.getAuthToken = function() { return storage.authToken; }
-
-    service.login = function(credentials) {
-      return AuthService.login(credentials).then(function(token) {
-        service.setAuthToken(token);
-      });
-    };
-
-    service.logout = function() {
-      return AuthService.logout().then(function() {
-        service.clear();
-      });
-    };
 
     service.isAuthentificated = function() {
       return service.getAuthToken() != undefined;
@@ -55,20 +82,30 @@ angular
       return storage.organization;
     };
 
-    // Persistency
-    service.restore = function() {
-      var currentUser = $window.localStorage.getItem('currentUser');
-      storage = JSON.parse(currentUser) || {};
-    };
-
-    service.clear = function() {
-      $window.localStorage.removeItem('currentUser');
-      storage = {};
-    };
-
     return service;
 
     //
+
+    function loadUser(userId) {
+      return UsersService.getUserById(userId)
+      .then(function(user) {
+        service.setUser(user);
+        return user;
+      });
+    }
+
+    function loadOrganizations(user) {
+      return OrganizationsService.getOrganizationsByUserId(user._id)
+      .then(function(orgs) {
+        return orgs;
+      });
+    }
+
+    function returnStorage(orgs) {
+      return $q(function(resolve, reject) {
+        resolve(storage);
+      });
+    }
 
     function persist() {
       $window.localStorage.setItem('currentUser', JSON.stringify(storage));
