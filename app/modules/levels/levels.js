d2e831e452dc6ed77716d7cea408d1e64efc3136
Corentin Kerisit <c@42.am>
2015-02-01 17:38:57 +0100

    Adding ability to buy level to the portal

--- app/modules/levels/levels.js
+++ app/modules/levels/levels.js
@@ -10,10 +10,42 @@ levels.config(function($stateProvider) {
 
 });
 
-levels.controller('LevelsController', function($scope, LevelsService) {
+levels.controller('LevelsController', function($q, $scope, LevelsService, CurrentUserService) {
 
-  LevelsService.getLevels().then(function(levels) {
+  var currentOrgId = $scope.currentUser.organization._id; // FIXME: Can be outdated (see app.run) ...
+  var levels = [];
+
+  $q.all([
+    LevelsService.getLevels(),
+    LevelsService.getLevelsByOrganizationId(currentOrgId)
+  ])
+  .then(function(results) {
+
+    levels = results[0];
+
+    var _boughtLevels = results[1];
+    var boughtLevels = [];
+
+    _.each(levels, function(level) {
+      var isAlreadyBought = _.find(_boughtLevels, function(boughtLevel) {
+        return level._id == boughtLevel._id;
+      });
+
+      if (isAlreadyBought)
+        level.bought = true;
+      else
+        level.bought = false;
+    });
+
     $scope.levels = levels;
   });
+
+
+  $scope.buyLevel = function(level) {
+    LevelsService.buyLevelbyOrganizationId(level._id, $scope.currentUser.organization._id).then(function() {
+      level.bought = true;
+      console.log('level '+level._id+' successfully bought !');
+    });
+  };
 
 });
