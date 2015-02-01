53be565ea109f1fa05b5edadec119d50e35311a5
Corentin Kerisit <c@42.am>
2015-02-01 17:38:32 +0100

    Adding Buy Level in LevelsService

--- app/modules/levels/services/LevelsService.js
+++ app/modules/levels/services/LevelsService.js
@@ -8,6 +8,31 @@ angular
       return Restangular.all('levels').getList(opts || {});
     };
 
+    service.getLevelsByOrganizationId = function(id, opts) {
+
+      opts = opts || {};
+      opts.embedded = JSON.stringify({
+          level: 1
+      });
+
+      return Restangular.all('organization-levels').getList(opts || {}).then(function(orgLevels) {
+        var levels = []
+
+        _.forEach(orgLevels, function(orgLevel) {
+          levels.push(orgLevel.level);
+        });
+
+        return levels;
+      });
+    };
+
+    service.buyLevelbyOrganizationId = function(levelId, orgId, opts) {
+      return Restangular.all('organization-levels').post({
+        organization: orgId,
+        level: levelId
+      });
+    };
+
     return service;
 
   });
