0d23b1d814b25e6b1af14468f4fcd342ce91b0e9
Corentin Kerisit <c@42.am>
2015-02-26 16:23:12 +0100

    Moving service delcaration into its own function

--- app/levels/LevelService.js
+++ app/levels/LevelService.js
@@ -1,71 +1,73 @@
+function LevelService(Restangular) {
+
+  var service = {};
+
+  service.getLevels = function(opts) {
+    return Restangular.all('levels').getList(opts || {});
+  };
+
+  service.getOrganizationLevel = function(organization, level) {
+    return Restangular.all('organization-levels')
+      .getList({
+        where: angular.toJson({
+          organization: organization._id,
+          level: level._id
+        })
+      })
+      .then(function(orgLevels) {
+        return orgLevels[0] || {};
+      });
+  }
+
+  service.getLevelsByOrganizationId = function(orgId, opts) {
+
+    opts = opts || {};
+    opts.embedded = JSON.stringify({
+        level: 1
+    });
+    opts.where = opts.where || {};
+    opts.where.organization = orgId;
+
+    return Restangular.all('organization-levels').getList(opts || {})
+      .then(function(orgLevels) {
+        var levels = [];
+
+        _.forEach(orgLevels, function(orgLevel) {
+          levels.push(orgLevel.level);
+        });
+
+        return levels;
+      });
+  };
+
+  // takes an object with a level attribute containing the level ID
+  service.getLevelInstances = function(opts) {
+    opts = opts || {};
+    opts.embedded = JSON.stringify({
+      server: 1
+    });
+
+    return Restangular.all('level-instances').getList(opts);
+  };
+
+  service.buyLevelbyOrganizationId = function(levelId, orgId, opts) {
+    return Restangular.all('organization-levels').post({
+      organization: orgId,
+      level: levelId
+    });
+  };
+
+  service.getLevel = function(levelId) {
+    return Restangular.one('levels', levelId).get()
+      .then(function(response) {
+        return response.data;
+      });
+  };
+
+  return service;
+
+}
+
 angular
   .module('portal.levels')
-  .factory('LevelService', function(Restangular) {
-
-    var service = {};
-
-    service.getLevels = function(opts) {
-      return Restangular.all('levels').getList(opts || {});
-    };
-
-    service.getOrganizationLevel = function(organization, level) {
-      return Restangular.all('organization-levels')
-        .getList({
-          where: angular.toJson({
-            organization: organization._id,
-            level: level._id
-          })
-        })
-        .then(function(orgLevels) {
-          return orgLevels[0] || {};
-        });
-    }
-
-    service.getLevelsByOrganizationId = function(orgId, opts) {
-
-      opts = opts || {};
-      opts.embedded = JSON.stringify({
-          level: 1
-      });
-      opts.where = opts.where || {};
-      opts.where.organization = orgId;
-
-      return Restangular.all('organization-levels').getList(opts || {})
-        .then(function(orgLevels) {
-          var levels = [];
-
-          _.forEach(orgLevels, function(orgLevel) {
-            levels.push(orgLevel.level);
-          });
-
-          return levels;
-        });
-    };
-
-    // takes an object with a level attribute containing the level ID
-    service.getLevelInstances = function(opts) {
-      opts = opts || {};
-      opts.embedded = JSON.stringify({
-        server: 1
-      });
-
-      return Restangular.all('level-instances').getList(opts);
-    };
-
-    service.buyLevelbyOrganizationId = function(levelId, orgId, opts) {
-      return Restangular.all('organization-levels').post({
-        organization: orgId,
-        level: levelId
-      });
-    };
-
-    service.getLevel = function(levelId) {
-      return Restangular.one('levels', levelId).get()
-        .then(function(response) {
-          return response.data;
-        });
-    };
-
-    return service;
-
-  });
+  .factory('LevelService', LevelService);
