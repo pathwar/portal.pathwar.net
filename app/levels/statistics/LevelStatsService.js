8cedd710ad67a38999951828616364c8d6ced296
Corentin Kerisit <c@42.am>
2015-02-27 19:41:30 +0100

    Adding level stats service

---
+++ app/levels/statistics/LevelStatsService.js
@@ -0,0 +1,25 @@
+function LevelStatsService(Restangular) {
+  var service = {};
+
+  service.getStatsForLevel = getStatsForLevel;
+
+  return service;
+
+  function getStatsForLevel(level) {
+    return Restangular.all('level-statistics')
+      .getList({
+        where: angular.toJson({
+          level: level._id
+        })
+      })
+      .then(function(stats) {
+        //TODO: Extend if stats is defined
+        level.statistics = stats;
+        return stats;
+      });
+  }
+}
+
+angular
+  .module('portal.levels')
+  .factory('LevelStatsService', LevelStatsService);
