02cc791893679413b7f598caa3a4ed25655cd0fe
Corentin Kerisit <c@42.am>
2015-03-01 19:38:29 +0100

    Remove members from organization page

--- app/organizations/OrganizationViewCtrl.js
+++ app/organizations/OrganizationViewCtrl.js
@@ -15,23 +15,6 @@ angular
 
       Orgs.one($stateParams.id).get().then(function(response) {
         vm.organization = Organization.build(response.data);
-
-        Restangular.all('organization-users')
-          .getList({
-            where: angular.toJson({
-              organization: vm.organization._id
-            }),
-            embedded: angular.toJson({
-              user: 1
-            })
-          })
-          .then(function(users) {
-            vm.organization.users = [];
-
-            for (var i = 0; i < users.length; i++) {
-              vm.organization.users.push(User.build(users[i].user));
-            }
-          });
       });
 
     }
