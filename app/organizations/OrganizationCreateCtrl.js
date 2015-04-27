bbc9cf7d625f073b476eafaf75c6e0b07b8a6737
Corentin Kerisit <c@42.am>
2015-04-27 15:58:54 +0200

    Image for sessions

--- app/organizations/OrganizationCreateCtrl.js
+++ app/organizations/OrganizationCreateCtrl.js
@@ -14,6 +14,16 @@ function OrganizationCreateCtrl(
   function init() {
     SessionService.getSessions().then(function(sessions) {
       vm.sessions = sessions;
+
+      //TODO: This should be in API
+      angular.forEach(vm.sessions, function(session) {
+        if (session.name.toLowerCase().indexOf('epitech') != -1) {
+          session.avatar = 'https://scontent-cdg.xx.fbcdn.net/hphotos-xtf1/v/t1.0-9/1454848_10152220512456116_345633387238949116_n.png?oh=4f6198124f61dece076236045f590617&oe=55E305C3';
+        }
+        else {
+          session.avatar = '//placehold.it/200x200';
+        }
+      });
     });
   }
 
