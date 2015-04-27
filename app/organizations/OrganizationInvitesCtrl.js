3ffc20e8a1abc723816cabdf9c2f788132cec0be
Corentin Kerisit <c@42.am>
2015-04-27 14:12:58 +0200

    Fetch pending and accepted request + display

--- app/organizations/OrganizationInvitesCtrl.js
+++ app/organizations/OrganizationInvitesCtrl.js
@@ -1,37 +1,64 @@
 function OrganizationInvitesCtrl(
-  $state, $stateParams, User, Restangular, CurrentUserService
+  $q, $state, $stateParams, User, Restangular, CurrentUserService
 ) {
   var vm = this;
-  vm.invites = [];
-
-  vm.organization = {};
+  //vm.pending_invites = null;
+  //vm.accepted_invites = null;
 
   init();
 
   function init() {
-
-    var currentOrg = CurrentUserService.getOrganization();
-
-    return Restangular.all('user-organization-invites').getList({
-      where: JSON.stringify({
-        organization: currentOrg._id,
-        status: 'pending'
-      }),
-      embedded: JSON.stringify({
-        user: 1
-      })
-    })
-    .then(function(invites) {
-      var _invites = [];
-      angular.forEach(invites, function(invite) {
-        invite.user = User.build(invite.user);
-        _invites.push(invite);
-      });
-      vm.invites = _invites;
-      return _invites;
-    });
-
+    fetchInvites();
   }
+
+  function fetchInvites() {
+      var currentOrg = CurrentUserService.getOrganization();
+
+      $q.all([
+        getInvitesByOrganizationId(currentOrg._id, {status: 'pending'}),
+        getInvitesByOrganizationId(currentOrg._id, {status: 'accepted'}),
+      ])
+      .then(function(results) {
+
+        var pending_invites = [];
+        var accepted_invites = [];
+
+        angular.forEach(results[0], function(invite) {
+          invite.user = User.build(invite.user);
+          pending_invites.push(invite);
+        });
+
+        angular.forEach(results[1], function(invite) {
+          invite.user = User.build(invite.user);
+          accepted_invites.push(invite);
+        });
+
+        vm.pending_invites = pending_invites;
+        vm.accepted_invites = accepted_invites;
+
+        return results;
+      });
+  }
+
+
+    //TODO: put this in a service
+    function getInvitesByOrganizationId(orgId, opts) {
+      var opts = opts || {};
+      var where = {
+        organization: orgId
+      };
+
+      if (opts.status) {
+        where.status = opts.status;
+      }
+
+      return Restangular.all('user-organization-invites').getList({
+        where: JSON.stringify(where),
+        embedded: JSON.stringify({
+          user: 1
+        })
+      });
+    }
 
 }
 
