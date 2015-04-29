ae35f8eab998645d93492629e518c8600f4458bf
Corentin Kerisit <c@42.am>
2015-04-29 18:39:14 +0200

    Adding Coupon directive

---
+++ app/coupons/CouponRedeemDirective.js
@@ -0,0 +1,18 @@
+function CouponRedeemDirective() {
+  var directive = {
+    restrict: 'EA',
+    templateUrl: 'coupons/views/redeem.tpl.html',
+    scope: {
+      level: '='
+    },
+    controller: 'CouponRedeemCtrl',
+    controllerAs: 'vm',
+    bindToController: true
+  };
+
+  return directive;
+}
+
+angular
+  .module('portal.coupons')
+  .directive('pwCouponRedeem', CouponRedeemDirective);
