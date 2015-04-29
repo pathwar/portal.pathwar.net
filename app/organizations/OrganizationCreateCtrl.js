74401125bf159604885330e4a61d6338e904a9a4
Corentin Kerisit <c@42.am>
2015-04-30 00:59:30 +0200

    Gray sessions where user already has organization

--- app/organizations/OrganizationCreateCtrl.js
+++ app/organizations/OrganizationCreateCtrl.js
@@ -1,5 +1,5 @@
 function OrganizationCreateCtrl(
-  $scope, $state, CurrentUserService, SessionService, OrganizationService,
+  $q, $state, CurrentUserService, SessionService, OrganizationService,
   Restangular, LoggerService
 ) {
   var vm = this;
@@ -13,11 +13,16 @@ function OrganizationCreateCtrl(
 
   /** Retrieve user sessions */
   function init() {
-    SessionService.getSessions().then(function(sessions) {
-      vm.sessions = sessions;
+    var user = CurrentUserService.getUser();
 
+    $q.all([
+      SessionService.getSessions(),
+      OrganizationService.getOrganizationsByUserId(user._id)
+    ]).then(function(results) {
+
+      var sessions = results[0];
       //TODO: This should be in API
-      angular.forEach(vm.sessions, function(session) {
+      angular.forEach(sessions, function(session) {
         if (session.name.toLowerCase().indexOf('epitech') != -1) {
           session.avatar = 'https://scontent-cdg.xx.fbcdn.net/hphotos-xtf1/v/t1.0-9/1454848_10152220512456116_345633387238949116_n.png?oh=4f6198124f61dece076236045f590617&oe=55E305C3';
         }
@@ -25,6 +30,17 @@ function OrganizationCreateCtrl(
           session.avatar = '//placehold.it/200x200';
         }
       });
+
+      var orgs = results[1];
+
+      angular.forEach(orgs, function(org) {
+        var sessionAlreadyIn = _.findWhere(sessions, {_id: org.session});
+        if (sessionAlreadyIn) {
+          sessionAlreadyIn.userHasOrganization = true;
+        }
+      });
+
+      vm.sessions = sessions;
     });
   }
 
