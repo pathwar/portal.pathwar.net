c4968268982305fe58b849d231e5d107efa58839
Corentin Kerisit <c@42.am>
2015-04-29 18:41:13 +0200

    Remove coupon state

--- app/coupons/coupons.js
+++ app/coupons/coupons.js
@@ -5,19 +5,4 @@ angular
     'ngAnimate',
     'restangular',
     'portal.services'
-  ])
-  .config(function($stateProvider) {
-
-    $stateProvider.state('coupons', {
-      url: '/coupons',
-      abstract: true,
-      template: '<div ui-view></div>'
-    })
-    .state('coupons.redeem', {
-      url: '/redeem',
-      controller: 'CouponRedeemCtrl',
-      controllerAs: 'vm',
-      templateUrl: 'coupons/views/redeem.tpl.html'
-    });
-
-  });
+  ]);
