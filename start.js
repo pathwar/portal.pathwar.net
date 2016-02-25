234ca38e2a0a4cdfcfef57b8fa5cd7e98e9e61e5
Corentin Kerisit <c@42.am>
2016-02-25 23:25:14 +0100

    Ugly temporary hack to enable development.
    While i find a solution for static genration and server side rendering.
    Signed-off-by: Corentin Kerisit <c@42.am>

--- start.js
+++ start.js
@@ -1,3 +1,5 @@
+import fs from 'fs'
+
 import webpack from 'webpack'
 import WebpackDevServer from 'webpack-dev-server'
 
@@ -7,6 +9,24 @@ const APP_PATH = process.cwd()
 const DEV_PORT = process.env.DEV_PORT || 8081
 
 function runDevServer() {
+
+  /*
+    ugly temporary hack so we can start dev whil I find a solution to couple
+    server side rendering and static generation.
+  */
+  if (!fs.existsSync('.build')) {
+    fs.mkdirSync('.build')
+  }
+  fs.writeFileSync('.build/index.html', `
+  <html>
+  <body>
+      <div id="app">Yo</div>
+      <script src="/vendor.js"></script>
+      <script src="/app.js"></script>
+  </body>
+  </html>
+  `)
+
   const compiler = webpack(ClientDevConfig)
   const server = new WebpackDevServer(compiler, ClientDevConfig.devServer)
   server.listen(DEV_PORT, 'localhost', () => {
