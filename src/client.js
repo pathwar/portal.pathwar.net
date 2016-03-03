8905b62228b8b15dbbb3c67fce9a7d6b6a9f08fd
Corentin Kerisit <c@42.am>
2016-03-03 11:24:11 +0100

    build: Adds linter
    Signed-off-by: Corentin Kerisit <c@42.am>

--- src/client.js
+++ src/client.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { render } from 'react-dom'
 
 class App extends React.Component {
-    render() {
-        return <div>Hello World</div>
-    }
+  render() {
+    return (<div>Hello World</div>)
+  }
 }
 
 render(
