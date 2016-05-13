c8631bfb7df1fcf3bcc084937278d6d956261d89
Corentin Kerisit <c@42.am>
2016-05-13 22:13:43 +0200

    Syncs redux with router

--- src/client.js
+++ src/client.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Router, browserHistory } from 'react-router'
+import { Provider } from 'react-redux'
+import { syncHistoryWithStore } from 'react-router-redux'
 
-import Root from './containers/Root'
 import configureStore from './store/configureStore'
+
+import routes from './routes'
+import DevTools from './containers/DevTools'
 
 const store = configureStore()
 
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store)
+
 render(
-  <Root store={store} />,
+  <Provider store={store}>
+    <div>
+      <Router history={history} routes={routes} />
+      <DevTools />
+    </div>
+  </Provider>,
   document.getElementById('root')
 )
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux'
+import { routerReducer as routing } from 'react-router-redux'
 
 function entities(state = { users: {}, levels: {} }, /*action*/) {
   return state
 }
 
 const rootReducer = combineReducers({
-  entities
+  entities,
+  routing,
 })
 
 export default rootReducer
